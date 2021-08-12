4400d2c692b48444c0ff7432b0685f52a7961906
Henrick Bueno <henrique.bueno@corbee.com.br>
2021-08-12 10:09:37 -0300

    aplicacao funcionando, faltando imprimir os creditos na tela

---
+++ classeQuery.php
@@ -0,0 +1,111 @@
+<?php
+    class liberaAcessoEmpresas {
+
+        //estanciar o pdo
+        private $pdo;
+
+        //função para conexao com o banco de dados
+        public function __construct($dbname, $user, $senha)
+        {
+            try { //parametros para o PDO = NOME DO BANCO DE DADOS, NOME DO USUARIO (DEFAULT ->ROOT), SENHA (DEFAULT "")
+                $this->pdo = new PDO("mysql:dbname=".$dbname,$user,$senha);
+            } catch (PDOException $th) {
+                echo "ERRO DE CONEXÃO COM O BANCO DE DADOS " . $th->getMessage();
+            } catch (Exception $th){
+                echo "ERRO GENÉRICO " . $th->getMessage();
+            }
+        }
+
+        // função para fazer o SELECT no bando de dados
+        public function buscaDadosEmpresas()
+        {
+            $select = $this->pdo->query("SELECT id, name, confirmated_at, created_at  FROM organizations");
+            return $select->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        // FUNCAO PARA COLOCAR A DATA DE HOJE NO BANCO DE DADOS
+        public function liberaEmpresa($id)
+        {   
+            $stmt = $this->pdo->prepare("UPDATE organizations SET confirmated_at = NOW() WHERE id = :id");
+            return $stmt->execute([
+                "id" => $id
+            ]);
+        }
+
+        //FUNCAO PARA TIRAR A DATA DO BANCO DE DADOS
+        public function bloqueiaEmpresa($id)
+        {
+            $stmt = $this->pdo->prepare("UPDATE organizations SET confirmated_at = null WHERE id = :id");
+            return $stmt->execute([
+                "id" => $id
+            ]);
+        }
+
+        // FUNCAO PARA ADICIONAR 7 DIAS
+        public function addSeteDias($id)
+        {
+            $stmt = $this->pdo->prepare('UPDATE organizations SET created_at = NOW() WHERE id = :id');
+            return $stmt->execute([
+                "id" => $id
+            ]);
+        }
+
+        public function buscaDadosUsuarios()
+        {
+            $select = $this->pdo->query("SELECT id, name, email, deleted_at, organization_id FROM users");
+            return $select->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function bloqueiaAcessoUsuario($id)
+        {
+            $stmt = $this->pdo->prepare("UPDATE users SET deleted_at = NULL WHERE id = :id");
+            return $stmt->execute([
+            "id" => $id
+            ]);
+        }
+
+        public function desbloqueiaAcessoUsuario($id)
+        {
+            $stmt = $this->pdo->prepare("UPDATE users SET deleted_at = NOW() WHERE id = :id");
+            return $stmt->execute([
+            "id" => $id
+            ]);
+        }
+
+        //QUERY PARA BUSCAR OS PACOTES DE CREDITOS
+        public function getPackets()
+        {
+            $packets = $this->pdo->query("SELECT id, name, qty, unitary_value, total_value, type, created_at, updated_at FROM data_packets");
+            return $packets->fetchAll(PDO::FETCH_ASSOC);
+        }
+        // QUERY PARA BUSCAR O ID DO PERFIL MASTER DA EMPRESA
+        public function getProfileMaster($id)
+        {
+            $stmt = $this->pdo->prepare("SELECT id FROM users WHERE profile_id IN (SELECT id FROM profiles WHERE name = 'Master' AND organization_id = :id) LIMIT 1");
+            $stmt->execute([
+                "id" => $id
+            ]);
+            return $stmt->fetch(PDO::FETCH_ASSOC)['id'];
+        }
+
+        //QUERO PARA INSERIR A COMPRA DE CREDITOS NO BANCO DE DADOS
+        public function creditaAmouth($organizationID, $user, $dataPacketId, $amountConsumed)
+        {
+            $stmt = $this->pdo->prepare("INSERT INTO organization_data_packets (
+                                                            organization_id, 
+                                                            user_id, 
+                                                            data_packet_id, 
+                                                            amount_consumed, 
+                                                            finished, 
+                                                            created_at, 
+                                                            updated_at) 
+                                                            VALUES (:organization_id, :user, :data_packet_id, :amount_consumed, '0', NOW(), NOW());");
+            return $stmt->execute([
+                "organization_id"   => $organizationID,
+                "user"              => $user,
+                "data_packet_id"    => $dataPacketId,
+                "amount_consumed"   => $amountConsumed
+            ]);
+        }
+
+    }
--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-    require_once 'classeLiberaEmpresas.php';
+    require_once 'classeQuery.php';
 
-    $newPdo     = new liberaAcessoEmpresas("organizations", "root", "");
+    $newPdo          = new liberaAcessoEmpresas("organizations", "root", "");
     $listaDeEmpresas = $newPdo->buscaDadosEmpresas();
+    $packets         = $newPdo->getPackets();
+    // echo '<pre>';
+    // var_dump($packets);
+    // echo '</pre>';
+    // exit;
     
     if (isset($_POST['id'])) {
         if ($_POST['action'] == 'bloquear') {
@@ -13,10 +18,11 @@
         if ($_POST['action'] == 'adicionar') {
             $resultado = $newPdo->addSeteDias($_POST['id']);
         } 
-        sleep(1);
+        // sleep(1);
         echo json_encode(['resultado' => $resultado]);
         exit;
     }
+
 ?>
 
 <!DOCTYPE html>
@@ -38,14 +44,39 @@
                 <li><a href="#">Lista de Empresas</a></li>
             </ol>
         </nav>
-        
+        <!-- INICIO DO MODAL -->
+        <div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Pacotes de Cr&eacute;ditos</h5>
+                    </div>
+                    <div class="modal-body">
+                        <Select id="pacotes" style="width: 45%;">
+                            <option value="Escolha o Plano" selected>Escolha o Plano</option>
+                            <?php foreach ($packets as $indice => $pacote) {?>
+                                <option value="<?php echo $pacote['id'] . ";" . $pacote['qty'] ?>"><?php echo $pacote["name"] ?></option>
+                            <?php } ?>
+                        </Select>
+                        <label for="creditosTotais" style="margin-left: 10%;">Cr&eacute;ditos Totais:</label>
+                        <input type="number" id="creditosTotais" value="">
+                        <input type="hidden" id="organizationId" value="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-success creditar" id="btn-creditar">Creditar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- FIM DO MODAL -->
         <div class="card-body container">
             <table class="table table-hover">
                 <thead class="thead-dark">
                     <tr>
                         <th> NOME </th>
                         <th> STATUS </th>
-                        <th style="text-align: center;">  PR&Oacute;XIMO BLOQUEIO DO TESTE: </th>
+                        <th style="text-align: center;">  PR&Oacute;XIMO BLOQUEIO / CR&Eacute;DITOS </th>
                         <th> <!-- ESPAÇO VAZIO PARA MANTER LAYOUT --></th>
                     </tr>
                 </thead> <?php 
@@ -72,7 +103,16 @@
                                             echo date('d/m/Y', strtotime('+ 7 days', strtotime($empresa['created_at'])));
                                         }else {
                                             echo '-';
-                                        }  ?>
+                                        }  
+                                    ?>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+                                    </svg>
+                                    <?php echo $pacote['qty'] ?>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                    </svg>
+                                    10000
                                 </td>
                                 <td style="text-align: center;">
                                     <button class="btn btn-danger bloquearAcesso" data-id="<?php echo $empresa['id']; ?>">BLOQUEAR ACESSO</button>
@@ -84,6 +124,9 @@
                                             </svg>
                                         </button>
                                     </a>
+                                    <button type="button" class="btn btn-success btnCreditar" data-id="<?php echo $empresa['id']; ?>" data-toggle="modal" data-target="#exampleModalCenter">
+                                        Cr&eacute;ditos
+                                    </button>
                                 </td><?php 
                             } else { ?>
                                 <td style="text-align: center;"> 
@@ -112,8 +155,79 @@
 
         
         <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="main.js"></script>
     </body>
 </html>
+<script>
+    // funcao para abrir o modal
+    $(".btnCreditar").click(function () {
+        $('#organizationId').val($(this).attr('data-id'));
+    })
 
+    // funcao para enviar o valor do select para o campo total de creditos
+    $('#pacotes').change(function () {
+        var idQty   = document.getElementById('pacotes').value;
+        var qty     = idQty.split(';');
+            if (qty[1]) {
+                document.getElementById('creditosTotais').value = qty[1];
+            }else{
+                document.getElementById('creditosTotais').value = '';
+            }
+   })
+
+    $("#btn-creditar").click(function () {
+        var orgId       = document.getElementById('organizationId').value;
+        var idQty       = document.getElementById('pacotes').value;
+        var qty         = idQty.split(';');
+        var id          = qty[0];
+        var valorPacote = qty[1];
+        var valor       = document.getElementById('creditosTotais').value;
+        if (valor > valorPacote) {
+            alert('O Valor creditado precisa ser menor do que o valor do pacote!');
+            return;
+        }
+        if (valor == valorPacote) {
+            ajaxCredito(id, 0, orgId, $('#btn-creditar').hasClass('creditar')? 'creditar' : 'parar');
+            return;    
+        }
+        if (valor < valorPacote) {
+            var valorAlterado = valorPacote - valor;
+            ajaxCredito(id, valorAlterado, orgId, $('#btn-creditar').hasClass('creditar')? 'creditar' : 'parar');
+            return;    
+        }
+    })
+
+    // Ajax 
+    function ajaxCredito(packetId, valor, organizationId, action) {
+        $.ajax({
+            url: 'ajaxCreditar.php',
+            type: 'post',
+            dataType: 'json',
+            data: {packetId, valor, organizationId, action},
+            beforeSend: function () {
+                if ($('#btn-creditar').hasClass('creditar')) {
+                    $('#btn-creditar').html('Creditando')
+                    $('#btn-creditar').attr("disabled", true)
+                }
+            },
+            success: function (response) {
+                console.log(response.masterProfileId);
+                location.reload();  
+            },
+            error: function () {
+                alert('Deu erro!');
+            },
+            complete: function() {
+                if ($('#btn-creditar').hasClass('creditar')) {
+                    $('#btn-creditar').html('Creditar')
+                    $('#btn-creditar').attr("disabled", true)
+                    alert('Creditado com sucesso!')
+                }
+            }
+        });
+    }
+    
+</script>
 
